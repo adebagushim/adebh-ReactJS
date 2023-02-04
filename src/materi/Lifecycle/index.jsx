db79496450222bc9caaee63eab5356e88b2601be
adebagus <adebagushim333@gmail.com>
2023-02-04 20:49:54 +0700

    Mantap

--- src/materi/Lifecycle/index.jsx
+++ src/materi/Lifecycle/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import axios from "axios";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Col, Row } from "react-bootstrap";
 import './index.css'
 
